0f95d27c5c5c63a8c01a06730ed12d8efbb68edc
LoungeXi <749124346@qq.com>
2025-03-28 23:15:56 +0800

    Site updated: 2025-03-28 23:15:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/28/hello-world copy/","2025/03/28/hello-world/"];function toRandomPost(){
+var posts=["2025/03/28/hello-world/","2025/03/28/hello-world copy/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
