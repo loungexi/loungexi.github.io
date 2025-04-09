3a65e9119ac7cc624ab1ca1cec86b333adc08578
LoungeXi <749124346@qq.com>
2025-04-09 21:09:58 +0800

    Site updated: 2025-04-09 21:09:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/27/后端开发之路/"];function toRandomPost(){
+var posts=["2025/03/27/后端开发之路 copy/","2025/03/27/后端开发之路/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
